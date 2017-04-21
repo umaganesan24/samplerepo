5d750d6f6a753b8401507d3d7c76d72098ca3b6c
gsaminathan <nathcathiram#1>
2017-04-21 14:46:24 -0700

    Intellij Commit

---
+++ HelloWorld/src/Helloworld2.java
@@ -0,0 +1,11 @@
+package HelloWorld.src;
+
+/**
+ * Created by gsaminathan on 4/21/17.
+ */
+public class Helloworld2 {
+    public static void main(String[] args) {
+        System.out.println("Hwllo World");
+    }
+
+}
